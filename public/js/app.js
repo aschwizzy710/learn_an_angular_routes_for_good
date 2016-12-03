316e5c2b7a26f91d4547ebc21607c4b7ee14b2b8
Aaron Schwartz <aschwartz628@gmail.com>
2016-12-02 19:50:05 -0500

    added the login view and login controller with ability to print email and password to the console

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,25 @@
 (function() {
-  angular.module('frontRoutes', []);  
+  angular.module('frontRoutes', ['ngRoute']) // inject the router
+        .config(function($routeProvider){
+          $routeProvider
+              .when("/", {
+                templateUrl: 'html/views/home.html',
+                controller: "MainController"
+              })
+              .when('/home', {
+                templateUrl: 'html/views/home.html',
+                controller: "MainController"
+              })
+              .when('/login', {
+                templateUrl: 'html/views/login.html',
+                controller: "LoginController"
+              })
+              .when('/signup', {
+                templateUrl: 'html/views/signup.html',
+                controller: "MainController"
+              })
+              .otherwise({
+                redirectTo:'/'
+              });
+        });
 }());
---
+++ public/js/controllers/login.controller.js
@@ -0,0 +1,15 @@
+(function() {
+  angular.module('frontRoutes')
+    .controller("LoginController", LoginController);
+
+  LoginController.$inject = ['$scope'];
+
+  function LoginController($scope){
+    $scope.login = login;
+
+
+    function login(user){
+      console.log(user);
+    }
+  }
+}());
